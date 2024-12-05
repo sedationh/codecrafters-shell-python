518edb874214dc4a4e8016de5f56db1f276ec400
SedationH <sedationh@gmail.com>
2024-12-05 23:35:47 +0800

    🔁 Refactor main loop to continuously prompt for commands

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    command = input()
-    sys.stdout.write(f"{command}: command not found\n")
+    while True:
+        sys.stdout.write("$ ")
+        command = input()
+        sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
