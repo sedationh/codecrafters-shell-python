ef7f4fc300a5ab7910a297ec8f0830331efff587
SedationH <sedationh@gmail.com>
2024-12-06 21:03:36 +0800

    ✨ Update command list in main loop to include 'exit' for graceful termination

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    commands = ["echo", "type"]
+    commands = ["echo", "type", "exit"]
 
     while True:
         sys.stdout.write("$ ")
