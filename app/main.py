903b48950e77c8f12ed170b2e24ef2d692d34e5e
SedationH <sedationh@gmail.com>
2024-12-06 21:58:21 +0800

    ✨ Enhance command handling in main loop

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import shutil
+import os
 
 
 def main():
@@ -8,34 +9,29 @@ def main():
         sys.stdout.write("$ ")
         command = input()
 
-        builtin_commands = ["echo", "type", "exit"]
+        builtin_commands = ["echo", "type", "exit", "pwd"]
 
-        if command == "exit 0":
-            break
-        # start with echo
-        if command.startswith("echo"):
-            sys.stdout.write(command[5:] + "\n")
-            continue
-        # start with type
-        if command.startswith("type"):
-            # type <command>
-            sub_command = command[5:]
-            if sub_command in builtin_commands:
-                sys.stdout.write(f"{sub_command} is a shell builtin\n")
-            else:
-                result = shutil.which(sub_command)
-                if result:
-                    sys.stdout.write(f"{sub_command} is {result}\n")
+        match command.split():
+            case ["exit", "0"]:
+                break
+            case ["echo", *args]:
+                sys.stdout.write(" ".join(args) + "\n")
+            case ["type", sub_command]:
+                if sub_command in builtin_commands:
+                    sys.stdout.write(f"{sub_command} is a shell builtin\n")
                 else:
-                    sys.stdout.write(f"{sub_command}: not found\n")
-            continue
-        if command.split() and shutil.which(command.split()[0]):
-            # Execute the command with arguments
-            args = command.split()
-            subprocess.run(args)
-            continue
-
-        sys.stdout.write(f"{command}: command not found\n")
+                    result = shutil.which(sub_command)
+                    if result:
+                        sys.stdout.write(f"{sub_command} is {result}\n")
+                    else:
+                        sys.stdout.write(f"{sub_command}: not found\n")
+            case ["pwd"]:
+                sys.stdout.write(f"{os.getcwd()}\n")
+            case [sub_command, *args] if shutil.which(sub_command):
+                # Execute the command with arguments
+                subprocess.run([sub_command, *args])
+            case _:
+                sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
