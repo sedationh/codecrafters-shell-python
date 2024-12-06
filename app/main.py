eb7a8c0ad02a17358e35ede8d55a4d03cbf5d21a
SedationH <sedationh@gmail.com>
2024-12-06 20:51:43 +0800

    ✨ Add exit command to break loop

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+        if command == "exit 0":
+            break
         sys.stdout.write(f"{command}: command not found\n")
 
 
